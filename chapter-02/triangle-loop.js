19880d0ca12cfd2b0e073e4eb5dbb79807c24d33
sevresbabylone <cheryl.francais@gmail.com>
2017-12-21 22:13:37 +0800

    adds triangle-loop

---
+++ chapter-02/triangle-loop.js
@@ -0,0 +1,12 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for (var i = 0; i <= 7; i++) {
+  console.log('#'.repeat(i))
+}
